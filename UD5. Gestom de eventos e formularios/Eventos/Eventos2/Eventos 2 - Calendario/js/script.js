3ccc58967d11b6cecdab92feb4c3d9766a03724e
guilherme <a19guillermobf@iessanclemente.net>
2022-02-14 23:30:22 +0100

    2022-02-14 Rematados os exercicios da UD5

--- UD5. Gestom de eventos e formularios/Eventos/Eventos2/Eventos 2 - Calendario/js/script.js
+++ UD5. Gestom de eventos e formularios/Eventos/Eventos2/Eventos 2 - Calendario/js/script.js
@@ -8,6 +8,13 @@
  * Mais fácil que fazer moitas comprovaçons?? Queda pa revisar
  */
 
+/** Recolho os elementos que podem ser usados para mostrar os resultados */
+let resultados = document.querySelector("#resultados");
+let cal = document.querySelector("#cal");
+/**Recolho os valores os input, se nom é um número, engade no id resultado "Proporcione um mes|ano correcto*/
+let mes = document.querySelector("#mes");
+let ano = document.querySelector("#ano");
+
 /** Funçom que determina se um ano é bissexto */
 function eBissexto(ano){
     let resultado=false;
@@ -25,26 +32,68 @@ function eBissexto(ano){
  * iterar de novo para ir metendo os dias da semana.
  * 
 */
-function taboa(array_data){
-    let tabla = "<table><thead><tr><th>Seg</th><th>Ter</th><th>Qua</th><th>Qui</th><th>Sex</th><th>Sab</th><th>Dom</th></tr></thead><tbody>"
-    for (let i = 0; i<array_data.length; i++){
-        tabla+="<tr>"
-        for (let j = 0; j< array_data[i].length; j++){
-            tabla+=`<td>${array_data[i][j]}</td>`
-        }
-        tabla+="</tr>"
+function taboa(array_data,ano,mes){
+    let nome_mes;
+    switch(mes){
+        case 0: nome_mes = "Janeiro";break;
+        case 1: nome_mes = "Fevereiro";break;
+        case 2: nome_mes = "Março";break;
+        case 3: nome_mes = "Abril";break;
+        case 4: nome_mes = "Maio";break;
+        case 5: nome_mes = "Juno";break;
+        case 6: nome_mes = "Julho";break;
+        case 7: nome_mes = "Agosto";break;
+        case 8: nome_mes = "Setembro";break;
+        case 9: nome_mes = "Outubro";break;
+        case 10: nome_mes = "Novembro";break;
+        case 11: nome_mes = "Dezembro";break;
     }
-    tabla+="<tbody></table>"
+    let tabla = document.createElement("table");
+    let titulo = document.createElement("caption");
+    titulo.appendChild(document.createTextNode(`${nome_mes} de ${ano}`))
+    tabla.appendChild(titulo);
+    let thead = document.createElement("thead");
+    thead.innerHTML = `<tr><th>Seg</th><th>Ter</th><th>Qua</th><th>Qui</th><th>Sex</th><th>Sab</th><th>Dom</th></tr>`
+    tabla.appendChild(thead);
+    //let tabla = `<table><caption>${nome_mes} de ${ano}</caption><thead><tr><th>Seg</th><th>Ter</th><th>Qua</th><th>Qui</th><th>Sex</th><th>Sab</th><th>Dom</th></tr></thead><tbody>`
+    for (let i = 0; i<array_data.length; i++){
+        //tabla+="<tr>"
+        let tr = document.createElement("tr")
+        for (let j = 0; j< array_data[i].length; j++){
+            //tabla+=`<td>${array_data[i][j]}</td>`
+            let td = document.createElement("td");
+            /**Se é domingo, engade a classe domingo ao td */
+            if(j==6 && array_data[i][j] != ""){
+                td.classList.add("domingo");
+            }
+            let valor = document.createTextNode(array_data[i][j]);
+            td.appendChild(valor);
+            tr.appendChild(td);
+        }
+        //tabla+="</tr>"
+        tabla.appendChild(tr)
+    }
+    //tabla+="<tbody></table>"
+    return tabla;
 }
 
 /** Funçom que devolve o último dia do mes */
 function fimMes(ano,mes) {
     let dia_fim_mes;
     switch(mes){
-        case 0,2,4,6,7,9,11:
+        case 0:
+        case 2:
+        case 4:
+        case 6:
+        case 7:
+        case 9:
+        case 11:
             dia_fim_mes = 31;
             break;
-        case 3,5,8,10:
+        case 3:
+        case 5:
+        case 8:
+        case 10:
             dia_fim_mes = 30;
             break;
         case 1:
@@ -63,71 +112,93 @@ let formulario = document.querySelector("#formulario");
 
 formulario.addEventListener("submit",evento=>{
     evento.preventDefault();
-    /**Recolho os valores os input, se nom é um número, engade no id resultado "Proporcione um mes|ano correcto*/
-    let mes = document.querySelector("#mes");
-    let ano = document.querySelector("#ano");
-
-    /** Co seguinte consigo que a umha data dada, com um fim de mes definido, ir gardando num array outros 
-     * arrays cos dias das semanas correspondentes
-     */
-
-    let fecha = new Date(ano.value,mes.value);
-    let dia_fim_mes = fimMes(ano.value,mes.value);
-    let array_mes = [];
-    let num_semana=0;
-    
-    /** Estabelece o dia a 1, e a condiçom para sair, que i seja false */
-    for (let i = true, num_dia=1; i !=false; ){
-        /**Crea o array semana onde se vam ir gardando os valores dos dias */
-        let semana = [];
-        /** Condicional em tres partes, para a primeira semana, se é é domingo (getDay() igual a 0), e é a última iteraçom
-         * mete o valor num_dia que vai ser 1, se nom, se o dia é outro e é maior que j+1, ou é domingo pero ainda nom é a últma iteraçom
-         * quere dizir que esse ia vai levar um valor valeiro, e se nom mete o valor do dia com normalidade.
-         */
-        if(num_semana == 0){
-            for(let j = 0; j<7;j++){
-                if (fecha.getDay()==0 && j==6){
-                    semana.push(num_dia);
-                    num_dia++;
-                }else if(fecha.getDay()>j+1 || fecha.getDay() == 0)	{
-                    semana.push("");
-                } else {
+ 
+    if (mes.value <= 0 || mes.value >= 13 || mes.value ==""){
+        resultados.textContent="Proporcione um mês correto"
+    } else if( ano.value == "" ){
+        resultados.textContent="Proporcione um ano correto"
+    } else {
+        /** Co seguinte consigo que a umha data dada, com um fim de mes definido, ir gardando num array outros 
+             * arrays cos dias das semanas correspondentes
+             */
+        let fecha = new Date(parseInt(ano.value),parseInt(mes.value)-1);
+        let dia_fim_mes = fimMes(parseInt(ano.value),parseInt(mes.value)-1);
+        let array_mes = [];
+        let num_semana=0;
+        
+        /** Estabelece o dia a 1, e a condiçom para sair, que i seja false */
+        for (let i = true, num_dia=1; i !=false; ){
+            /**Crea o array semana onde se vam ir gardando os valores dos dias */
+            let semana = [];
+            /** Condicional em tres partes, para a primeira semana, se é é domingo (getDay() igual a 0), e é a última iteraçom
+             * mete o valor num_dia que vai ser 1, se nom, se o dia é outro e é maior que j+1, ou é domingo pero ainda nom é a últma iteraçom
+             * quere dizir que esse ia vai levar um valor valeiro, e se nom mete o valor do dia com normalidade.
+             */
+            if(num_semana == 0){
+                for(let j = 0; j<7;j++){
+                    if (fecha.getDay()==0 && j==6){
+                        semana.push(num_dia);
+                        num_dia++;
+                    }else if(fecha.getDay()>j+1 || fecha.getDay() == 0)	{
+                        semana.push("");
+                    } else {
+                        semana.push(num_dia);
+                        num_dia++;
+                    }
+                }
+            /** Se o dia de fim de mes, menos o numero de dia polo que imos restados dam menos de 7 quere dizir que estamos na última semana
+             * polo que se o numero de dia é igual ou menor que o número do fim de mes, mete valores com normalidade, se nom mete valeiros
+             * Ao final estabelece i a false para que remate o bucle principal
+             */
+            } else if(dia_fim_mes-num_dia < 7) {
+                for(let j=0;j<7;j++){
+                    if(num_dia<=dia_fim_mes){
+                        semana.push(num_dia);
+                        num_dia++;
+                    } else {
+                        semana.push("");
+                    }
+                }
+                i=false;
+            /** Mentras vai ir metendo valores de 7 em 7 */
+            } else {
+                for(let j=0;j<7;j++){
                     semana.push(num_dia);
                     num_dia++;
                 }
             }
-        /** Se o dia de fim de mes, menos o numero de dia polo que imos restados dam menos de 7 quere dizir que estamos na última semana
-         * polo que se o numero de dia é igual ou menor que o número do fim de mes, mete valores com normalidade, se nom mete valeiros
-         * Ao final estabelece i a false para que remate o bucle principal
-         */
-        } else if(dia_fim_mes-num_dia < 7) {
-            for(let j=0;j<7;j++){
-                if(num_dia<=dia_fim_mes){
-                    semana.push(num_dia);
-                    num_dia++;
-                } else {
-                    semana.push("");
-                }
-            }
-            i=false;
-        /** Mentras vai ir metendo valores de 7 em 7 */
-        } else {
-            for(let j=0;j<7;j++){
-                semana.push(num_dia);
-                num_dia++;
-            }
+            num_semana++;
+            array_mes.push(semana);
         }
-        num_semana++;
-        array_mes.push(semana);
+        /**Se cal tem algo metido, borrao */
+        if (cal.firstChild) {cal.removeChild(cal.firstChild)};
+        /**Se resultados tem algo metido, borra-o */
+        if (resultados.firstChild) {resultados.removeChild(resultados.firstChild)};
+        /**Inclue a tábua no html */
+        cal.appendChild(taboa(array_mes,parseInt(ano.value),parseInt(mes.value)-1));
     }
-    console.log(array_mes);
+})
 
-    /**
-     * fazer a comprovaçom dos input, que seja um número, que os meses nom sejam maiores que 11 e essas cousas
-     * igual fazendo um input number com limitaçom por riba e por baixo já valeria
-     * assi já namais haveria que verificar que nom esteja valeiro
-     * 
-     * Meter os resultados, no html
-     */
+/**Isto parece que esta metendo bem as cousas. Agora, faltaria o evento focus nos imput, porque
+ * parece no video que cando clicka enriba dos input borra todo o que hai embaixo
+ */
 
+mes.addEventListener("focus",evento=>{
+    evento.preventDefault();
+    /**Se cal tem algo metido, borrao */
+    if (cal.firstChild) {cal.removeChild(cal.firstChild)};
+    /**Se resultados tem algo metido, borra-o */
+    if (resultados.firstChild) {resultados.removeChild(resultados.firstChild)};
+    /**E o value do input tamem o cambia*/
+    mes.value="";
+})
+
+ano.addEventListener("focus",evento=>{
+    evento.preventDefault();
+    /**Se cal tem algo metido, borrao */
+    if (cal.firstChild) {cal.removeChild(cal.firstChild)};
+    /**Se resultados tem algo metido, borra-o */
+    if (resultados.firstChild) {resultados.removeChild(resultados.firstChild)};
+    /**E o value do input tamem o cambia*/
+    ano.value="";
 })
