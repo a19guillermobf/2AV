e7fe3a28cf7e570522e00384de2e8bbef30d9886
dawMP - Brea Fariña <a19guillermobf@a21eql08.sanclemente.local>
2022-03-08 22:10:33 +0100

    2022-03-08 Continuando co de datos de alunos

--- UD6. Modelo de objetos do documento/Registro de datos/Registro de Datos - Alumnos/scripts/myscript.js
+++ UD6. Modelo de objetos do documento/Registro de datos/Registro de Datos - Alumnos/scripts/myscript.js
@@ -15,68 +15,7 @@
  * Polo que apuntei na outra tarefa, esta classe teria estes métodos e ademais teria internamente a estrutura de datos 
  */
 
-/**A classe para a interfaze, entendo que só vai ter que ter a estrutura de dados, que vai ser algo do estilo array/coleçom de classes de 
- * alunado. E ademais, vai ser a que interatue co HTML. Terá que recolher os datos da sessom, modificar o html, crear o fragmento esse?
- */
-class controlador{
-    constructor(alunos){
-        this.alunos = alunos;
-    }
-    /**Recolhe os elementos que hai no HTML creando umha instancia 
-     * da classe aluno
-    */
-    recolheDatosHTML(){
-        /**Recolhe um string */
-        let nif = document.querySelector("#nif").value;
-        /**Recolhe um string */
-        let nome = document.querySelector("#nombre").value;
-        /**Este vai revisar se o sexo Home está marcado, se está colhe o valor desse elemento, se nom colhe o de mulher
-         * Haveria que se assegurar de que cando se chama a esta funçom todos os campos estám cubertos,
-         * ainda que polo que se ve no vídeo, nalgúm momento hai que fazer que Hombre esteja marcado por defecto
-         */
-        let sexo = document.getElementsByName("sexo")[0].checked ? document.getElementsByName("sexo")[0].value : document.getElementsByName("sexo")[1].value;
-        /**Recolhe um string */
-        let enderezo = document.querySelector("#direccion").value;
-        /**Recolhe um valor do 0 ao 5, depois entendo que haveria que relacionar co string correspondente
-         * com por exemplo document.querySelectorAll("option")[3].textContent, sendo 3 o número que recolhemos em estudo
-         */
-        let estudos = document.querySelector("#nombre").value;
-        /**String ou int */
-        let telefone = document.querySelector("#telefono").value;
-        /**String */
-        let mail = document.querySelector("#email").value;
-        /**Agora, para os checkbox, com document.querySelectorAll('input[type="checkbox"]') ou com document.getElementsByName("aficion")
-         *  seleccionam-se os tres, haveria que ver quais som os 
-         * checked. Tamém haveria que ver se "Otra" tem algum contido.
-         * Estes valores haveria que garda-los numha estrutura de datos, por exemplo um array ou assi
-         * Entom, igual podo crear umha variável pa recolher os checkbox, e logo a variável hobis, array onde ir metendo os que correspondam
-         */
-        let checkes = document.getElementsByName("aficion");
-        let hobis = [];
-        checkes.forEach(elemento => {
-            if (elemento.checked){
-                
-            }
-        });
-    }
-    actualizaDatos(){
-
-    }
-    actualizaHTML(){
-
-    }
-    reseteaFormulario(){
-
-    }
-}
-
-
-
-//document.querySelector("#nif").value
-document.getElementsByName("sexo")
-//document.getElementsByName("sexo")[0].checked
-
-class aluno{
+ class Aluno{
     //Construtor ao que se lhe passam todos os campos
     constructor(nif,nome,sexo,enderezo,data,estudos,telefone,mail,hobis){
         this.nif = nif;
@@ -149,3 +88,126 @@ class aluno{
         this.hobis = hobis;
     }
 }
+
+/**Cambio de tercio por sugerencia de Julian, a parte que ia fazer com isto
+ * melhor faze-la primeiro com um arrai onde ir gardando as classes e 
+ * funçons pa ir fazendo as cousas.
+ */
+let alunos = [];
+let formulario = document.querySelector("form");
+/**Recolhe os elementos que hai no HTML 
+*/
+    /**Recolhe um string */
+    let nif = document.querySelector("#nif");
+    /**Recolhe um string */
+    let nome = document.querySelector("#nombre");
+    /**Este vai revisar se o sexo Home está marcado, se está colhe o valor desse elemento, se nom colhe o de mulher
+     * Haveria que se assegurar de que cando se chama a esta funçom todos os campos estám cubertos,
+     * ainda que polo que se ve no vídeo, nalgúm momento hai que fazer que Hombre esteja marcado por defecto
+     */
+    //let sexo = document.getElementsByName("sexo")[0].checked ? document.getElementsByName("sexo")[0].value : document.getElementsByName("sexo")[1].value;
+    let sexo = document.getElementsByName("sexo")
+    /**Recolhe um string */
+    let enderezo = document.querySelector("#direccion");
+    /** Recolhe a data de nacemento */
+    let data = document.querySelector("#fnac");
+    /**Recolhe um valor do 0 ao 5, depois entendo que haveria que relacionar co string correspondente
+     * com por exemplo document.querySelectorAll("option")[3].textContent, sendo 3 o número que recolhemos em estudo
+     */
+    let estudos = document.querySelector("#estudios");
+    /**String ou int */
+    let telefone = document.querySelector("#telefono");
+    /**String */
+    let mail = document.querySelector("#email");
+    /**Agora, para os checkbox, com document.querySelectorAll('input[type="checkbox"]') ou com document.getElementsByName("aficion")
+     *  seleccionam-se os tres, haveria que ver quais som os 
+     * checked. Tamém haveria que ver se "Otra" tem algum contido.
+     * Estes valores haveria que garda-los numha estrutura de datos, por exemplo um array ou assi
+     * Entom, igual podo crear umha variável pa recolher os checkbox, e logo a variável hobis, array onde ir metendo os que correspondam
+     */
+    let aficions = document.getElementsByName("aficion");
+    let outra = document.getElementsByName("otra");
+
+
+/**Resetea formulario e asigna os valores que tem que ter 
+ * Em primeiro lugar, tem que borrar todos os textbox
+ * tem que por a disabled todos os campos excepto o do nif
+ * Tem que seleccionar Hombre e Sin estudios, e desseleccionar todos os de aficiones
+ */
+
+function reseteaFormulario(){
+    for (let i = 2; i < formulario.length-1; i++){
+        formulario[i].disabled = true
+    }
+    nif.value="";
+    nome.value="";
+    sexo[0].checked=true;
+    enderezo.value="";
+    estudos.value=0;
+    telefone.value="";
+    mail.value="";
+    outra.value="";
+    aficions.forEach(elemento =>{
+        elemento.checked=false;
+    })
+}
+
+/**Garda o array de alunos em sessom */
+function gardaDatosSessom(){
+    sessionStorage.setItem("alunos",JSON.stringify(alunos))
+}
+
+/**Recupera os datos de alunos gardados em sessom 
+ * e crea o array alunos */
+function recuperaDatosSessom(){
+    /**Se os datos estam gardados recupera-os */
+    if(sessionStorage.getItem("alunos")){
+        /**Com este parse converte a cadea de texto que tinhamos gardada
+         * à estrutura de datos JSON ca que trabalhamos
+         */
+        alunos = JSON.parse(sessionStorage.getItem("alunos"))
+    }
+}
+
+/**"Pinta" a tábua HTML cos datos gardados no array alunos */
+function pintaTabua(){
+
+}
+
+/**Engade um aluno à estrutura de datos */
+function engadeAluno(){
+    /**No campo de sexo, gardo se Hombre está selecionado ou nom 
+     * Depois à hora de recuperar, se é true marca-se este como chkecked true
+     * e se é false marcase Mujer como checked true
+    */
+   /**No campo de hobis, terei que gardar os valores de cada label
+    * que é o seguinte irmao. Tamém haverá que gardar o valor de Otra
+    * A dúvida é, se o gardo todo junto, depois para recupera-lo é umha liada,
+    * Igual nom está demais gardar o de Otra aparte e à hora de pintar a tabla,
+    * meter estes contidos juntos
+   */
+   let hobbies = [];
+    aficions.forEach(elemento => {
+        if (elemento.checked){
+            hobbies.push(elemento.nextSibling.textContent);
+        }
+    });
+    if (document.getElementsByName("otra")[0].value != "" ){
+        hobis.push(document.getElementsByName("otra")[0].value)
+    }
+    const novoaluno = new Aluno(nif.value,sexo[0].checked,enderezo.value,data.value,estudos.value,telefone.value,mail.value,hobis)
+    alunos.push(novoaluno);
+}
+
+/** 
+    actualizaDatos(){
+
+    }
+    actualizaHTML(){
+
+    }
+    reseteaFormulario(){
+
+    }
+
+*/
