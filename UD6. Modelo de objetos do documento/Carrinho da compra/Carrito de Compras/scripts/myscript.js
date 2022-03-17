8f77d598ac22d5b0343d173f0019a09b6729daeb
guilherme <a19guillermobf@iessanclemente.net>
2022-03-17 23:52:19 +0100

    2022-03-17 Rematado o exercicio do carrinho de compras e preparado para começar o de Mamma Mia

--- UD6. Modelo de objetos do documento/Carrinho da compra/Carrito de Compras/scripts/myscript.js
+++ UD6. Modelo de objetos do documento/Carrinho da compra/Carrito de Compras/scripts/myscript.js
@@ -1,18 +1,4 @@
-/**Primeiro de todo, hai que carregar os datos no div com id="lista-productos"
- * Dentro hai que meter cada objeto na template seguinte
- * <template id="template-producto">
-            <div class="col-12 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center">
-                <div class="card">
-                    <img src="" class="card-img-top" alt="item-producto">
-                    <div class="card-body">
-                        <h5 class="card-title"></h5>
-                        <p class="card-text"></p>
-                        <a href="#" class="btn btn-dark">Comprar</a>
-                    </div>
-                </div>
-            </div>
-        </template>   
- */
+/**Incluo na lista de produtos outro campo mais chamado ud que representaria as unidades engadidas de cada pruduto*/
 let productos = [
     {
         "precio": 500,
@@ -58,10 +44,20 @@ let productos = [
     }
 ]
 
+/**Recolhe o div onde vai ir a lista de produtos*/
 let listaProdutos = document.querySelector("#lista-productos");
-/**Recolhe a template para a lista de produtos*/
-listaProdutosTemplate = document.querySelector('#template-producto').content
+/** Recolhe os anacos da tábua carrinho*/
+let tbodycarrinho = document.querySelector("#body-carrito");
+let tfootercarrinho = document.querySelector("#footer-carrito");
+
+/**Introduze do html os cards dos produtos 
+ * Primeiro de todo, hai que carregar os datos no div com id="lista-productos"
+ * Dentro hai que meter cada objeto na template correspondente   
+ *
+*/
 function pintaProdutos(){
+    /**Recolhe a template de um produto para a lista de produtos*/
+    let listaProdutosTemplate = document.querySelector('#template-producto').content
     /**Crea um fragmento onde vai ir gardando os datos */
     let fragmento = document.createDocumentFragment();    
     /**Percorre todos os produtos, e em cada um fai um clone da teplate, 
@@ -70,9 +66,7 @@ function pintaProdutos(){
      */
     productos.forEach(produto => {
         productoTemplate = listaProdutosTemplate.cloneNode(true);
-        console.log(produto.thumbnailUrl)
-        productoTemplate.querySelector("img").scr=produto.thumbnailUrl;
-        console.log(productoTemplate.querySelector("img").scr);
+        productoTemplate.querySelector("img").src=produto.thumbnailUrl;
         productoTemplate.querySelector("h5").textContent=produto.title;
         productoTemplate.querySelector("p").textContent=`${produto.precio} €`;
         productoTemplate.querySelector("a").dataset.id=produto.id;
@@ -80,34 +74,13 @@ function pintaProdutos(){
     });
 
     listaProdutos.appendChild(fragmento);
-    /**Por algum motivo que nom comprendo, nom me carrega bem as imagens 
-     * no anterior bucle, tenho que meter um novo para as imagens depois
-     * de crear e engadir o fragmento ao html
-     */
-    let a = 0;
-    productos.forEach(produto => {
-        listaProdutos.querySelectorAll("img")[a].src=produto.thumbnailUrl;
-        a++;
-    })
 }
 
 /**Agora que já se mostram os dados no html, e estam os botons associados a
  * umha id, que é o seguinte?
  * 
- * Podem ser duas funçons e um mesmo evento, click
- * Creando umha nova estrutura de datos, que garde os produtos que hai
- * engadidos, com um campo a maiores, que indique o número de unidades
- * que se engadirom.
- * Logo umha funçom, à que se lhe passe um id, que nessa estrutura de 
- * datos inclua o elemento se nom existe e que aumente o número de unidades. 
- * (Ou mesmo utilizar a mesma estrutura que temos já feita?)
- * 
- * E outra igual pero para, retirar produtos (ou igual a mesma pode fazer
- * as duas cousas??)
- * 
- * Logo, de algum jeito haveria que ir atualizando as cantidades
- * E também gardar os dados na sessom suponho
- */
+*/
+
 
 function recuperaDatosSessom(){
     /**Coma sempre, se hai datos gardados recupera-os e 
@@ -122,10 +95,101 @@ function gardaDatosSessom(){
     sessionStorage.setItem("produtos", JSON.stringify(productos));
 }
 
-/**Esta funçom, passa-se-lhe o id de um produto, e o engade
- * seria um booleano, se é true é para sumar
- * se é false para restar
- */
-function modificaTabuaProdutos(id,engade){
-
+/**Funçom que engade ou resta umha unidade a um produto, logo com a seguinte
+ * pinto a parte do carrinho, se as unidades som 0 já nom as pinta.
+ * Passa-se-lhe umha id de produto e um engade, que vai ser um booleano, se é true engade, se é false retira
+*/
+function modificaUnidadesProduto(id,engade){
+    productos.forEach(produto=>{
+        if(produto.id == id){
+            console.log(produto.ud)
+            engade ? produto.ud++ : produto.ud--;
+            console.log(produto.ud)
+        }
+    })
 }
+
+/**Funçom que pom a 0 todos os campos ud de todos os produtos */
+function reseteaUdsProdutos(){
+    productos.forEach(produto=>{
+        produto.ud = 0;
+    })
+}
+
+/** Funçom que pinta a tábua */
+function pintaTabuaCarrinho(){
+    /**Borra o contido que exista inicialmente */
+    tbodycarrinho.innerHTML="";
+    tfootercarrinho.innerHTML="";
+    /** Variáveis dos totais de unidades e preço que se iram incrementando
+     * por cada produto que se engada
+     */
+    let totaluds = 0;
+    let total = 0;
+    /**Número de item para o promeiro campo do tbody */
+    let num = 1;
+    /**Recolhem-se as templates  */
+    let templateProdutoCarrinho = document.querySelector("#template-producto-carrito").content;
+    let templateFooterCarrinho = document.querySelector("#template-footer-carrito").content; 
+    /**Crear os fragmentos */
+    let fragmentoProduto = document.createDocumentFragment(true);
+    let fragmentoFooter = document.createDocumentFragment(true);
+    /**Percorre os produtos, os que tenham ud > 0 serám engadidos à tábua */
+    productos.forEach(produto=>{
+        if(produto.ud > 0){
+            let temporal = templateProdutoCarrinho.cloneNode(true);
+            let tds = temporal.querySelectorAll("td");
+            tds[0].textContent = num;
+            num++;
+            tds[1].textContent = produto.title;
+            tds[2].textContent = produto.ud;
+            tds[3].querySelectorAll("button")[0].dataset.id = produto.id;
+            tds[3].querySelectorAll("button")[1].dataset.id = produto.id;
+            tds[4].textContent = `${produto.ud*produto.precio} €`;
+            totaluds+=produto.ud;
+            total+=produto.ud*produto.precio;
+            fragmentoProduto.appendChild(temporal);
+        }
+    })
+    tbodycarrinho.appendChild(fragmentoProduto);
+    if(totaluds>0){
+        let temporalfooter = templateFooterCarrinho.cloneNode(true);
+        let tds = temporalfooter.querySelectorAll("td");
+        tds[0].textContent = totaluds;
+        tds[2].textContent = `${total} €`;
+        fragmentoFooter.appendChild(temporalfooter);
+        tfootercarrinho.appendChild(fragmentoFooter);
+    } else {
+        tbodycarrinho.innerHTML='<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>';
+    }
+}
+
+
+/**Executa as funçons no inicio */
+document.addEventListener("DOMContentLoaded",e=>{
+    recuperaDatosSessom();
+    pintaProdutos();
+    pintaTabuaCarrinho();
+})
+
+
+/**com elemento.classList.contains("classe") podo saber se esse elemento contem umha determinada classe.
+ * Útil para comprovar agora que botom se preme
+ */
+document.addEventListener("click",evento=>{
+    /**Se se preme no botom de comprar ou no de +, aumenta umha unidade esse produto */
+    console.log(evento.target)
+    if(evento.target.classList.contains("btn-dark") || evento.target.classList.contains("btn-info")){
+        modificaUnidadesProduto(evento.target.dataset.id,true);
+    /**Se se preme o botom com id vaciar-carrito, pom a 0 todos os valores de ud dos produtos, este tem que vir antes
+     * pois tem a mesma classe que o botom -
+     */
+    } else if(evento.target == document.querySelector("#vaciar-carrito")) {
+        reseteaUdsProdutos();
+    /**Se se preme o botom - resta umha unidade */
+    } else if(evento.target.classList.contains("btn-danger")) {
+        modificaUnidadesProduto(evento.target.dataset.id,false);
+    }
+    pintaTabuaCarrinho();
+    gardaDatosSessom();
+})
